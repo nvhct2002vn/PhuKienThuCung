3093931738887987a90065eeb0643b136f385f55
nvhct2002vn <viethien123vn@gmail.com>
2022-07-15 21:59:47 +0700

    delete,update qty, clear prd on localStorage

--- src/main/java/com/example/pet/controller/CartController.java
+++ src/main/java/com/example/pet/controller/CartController.java
@@ -1,5 +1,12 @@
 package com.example.pet.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class CartController {
-	
+	@RequestMapping("/cart")
+	public String cart() {
+		return "/views/main/cart";
+	}
 }
--- src/main/resources/static/assets/js/shopping-cart.js
+++ src/main/resources/static/assets/js/shopping-cart.js
@@ -1,24 +1,47 @@
-const app = angular.module("shopping-cart-app", []).controller("shopping-cart-ctrl", function ($scope, $http) {
-    $scope.cart = {
-        items: [],
-        add(id) {
-            var item = this.items.find(item => item.id == id)
-            if (item) {
-                item.qty++;
-                this.saveToLocalStorage();
-            } else {
-                $http.get(`/api/product/${id}`).then(result => {
-                    result.data.qty = 1;
-                    this.items.push(result.data);
+const app = angular.module("app", []).controller("shopping-cart-ctrl", function ($scope, $http) {
+    $scope.quantity,
+
+        $scope.cart = {
+            items: [],
+            add(id) {
+                var item = this.items.find(item => item.id == id)
+                if (item) {
+                    item.qty += $scope.quantity;
                     this.saveToLocalStorage();
-                })
+                } else {
+                    $http.get(`/api/product/${id}`).then(result => {
+                        result.data.qty = $scope.quantity;
+                        this.items.push(result.data);
+                        this.saveToLocalStorage();
+                    })
+                }
+            },
+            saveToLocalStorage() {
+                var json = JSON.stringify(angular.copy(this.items));
+                localStorage.setItem("cart", json);
+                console.log(localStorage.getItem("cart"));
+            },
+            loadFromLocalStorage() {
+                var json = localStorage.getItem("cart");
+                this.items = json ? JSON.parse(json) : [];
+            },
+            //get số lượng sp trong local
+            get count() {
+                return this.items.map(item => item.qty).reduce((total, qty) => total += qty, 0);
+            },
+            //get tổng tiền trong local        
+            get amount() {
+                return this.items.map(item => item.qty * item.price).reduce((total, qty) => total += qty, 0);
+            },
+            remove(id) {
+                var index = this.items.findIndex(item => item.id == id);
+                this.items.splice(index, 1);
+                this.saveToLocalStorage();
+            },clear(){
+                localStorage.clear();
+                $scope.cart.loadFromLocalStorage();
             }
-        },
-        saveToLocalStorage() {
-            var json = JSON.stringify(angular.copy(this.items));
-            localStorage.setItem("cart", json);
-            console.log(localStorage.getItem("cart"));
         }
-    }
+    $scope.cart.loadFromLocalStorage();
 })
 
